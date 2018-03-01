728903600b58ebee398912e678c36983977f077e
Alex Welk <alexwelk.92@gmail.com>
2018-02-28 20:53:11 -0500

    Initial commit

---
+++ src/main/java/org/_3rev/peachtreecurling/PeachtreeCurlingApplication.java
@@ -0,0 +1,12 @@
+package org._3rev.peachtreecurling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeachtreeCurlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeachtreeCurlingApplication.class, args);
+	}
+}
